e917d57ef352b0c2b716988eec6030313acc0d79
ihuunter <lbake079@gmail.com>
2025-07-06 15:57:34 +0300

    Delete apps/api directory

--- apps/api/src/routes/oembed/helpers/meta/getMetaContent.ts
+++
@@ -1,9 +0,0 @@
-const getMetaContent = (document: Document, name: string): string | null => {
-  const metaTag =
-    document.querySelector(`meta[name="${name}"]`) ||
-    document.querySelector(`meta[property="${name}"]`);
-
-  return metaTag ? metaTag.getAttribute("content") : null;
-};
-
-export default getMetaContent;
